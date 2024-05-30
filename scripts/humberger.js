4a01dae4458a7887305a5303e5673c29bd6b10b7
Yukari Hall <yukarihall@gmail.com>
2024-05-30 16:58:49 -0600

    humberger change

--- scripts/humberger.js
+++ scripts/humberger.js
@@ -1,7 +1,14 @@
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('#navMenu');
+let hamButton = document.querySelector('#menu');
+let navigation = document.querySelector('#navMenu');
 
 hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
+	// let navigation = document.querySelector('.humbergerItems')
+	// navigation.classList.toggle('open');
+	// hamButton.classList.toggle('open');
+	if (navigation.style.display==="flex"){
+		navigation.style.display="none";
+	}
+	else{
+		navigation.style.display="flex";
+	}
 });
