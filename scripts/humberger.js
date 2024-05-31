55fef80e63291d369f29e4f555c4310dbb4be150
Yukari Hall <yukarihall@gmail.com>
2024-05-30 22:06:21 -0600

    miner change

--- scripts/humberger.js
+++ scripts/humberger.js
@@ -1,14 +1,22 @@
-let hamButton = document.querySelector('#menu');
-let navigation = document.querySelector('#navMenu');
+// let hamButton = document.querySelector('#menu');
+// let navigation = document.querySelector('#navMenu');
 
-hamButton.addEventListener('click', () => {
-	// let navigation = document.querySelector('.humbergerItems')
-	// navigation.classList.toggle('open');
-	// hamButton.classList.toggle('open');
-	if (navigation.style.display==="flex"){
-		navigation.style.display="none";
-	}
-	else{
-		navigation.style.display="flex";
-	}
+// hamburger.addEventListener('click', () => {
+// 	// let navigation = document.querySelector('.humbergerItems')
+// 	// navigation.classList.toggle('open');
+// 	// hamButton.classList.toggle('open');
+// 	if (navigation.style.display==="flex"){
+// 		navigation.style.display="none";
+// 	}
+// 	else{
+// 		navigation.style.display="flex";
+// 	}
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+	const hamButton = document.querySelector('#menu');
+  const navigation = document.querySelector('#navMenu');
+	hamButton.addEventListener('click', () => {
+		navigation.classList.toggle('open');
+	});
 });
