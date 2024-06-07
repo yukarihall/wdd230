f1c5b90873a94de3bb40978885963bddb84b7073
Yukari Hall <yukarihall@gmail.com>
2024-06-06 21:16:26 -0600

    visitors

---
+++ lesson08/scripts/api.js
@@ -0,0 +1,38 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+  displayList(chapter);
+});
+
+button.addEventListener('click', () => {
+  if (input.value != '') {  // make sure the input is not empty
+    displayList(input.value); // call the function that outputs the submitted chapter
+    chaptersArray.push(input.value);  // add the chapter to the array
+    setChapterList(); // update the localStorage with the new array
+    input.value = ''; // clear the input
+    input.focus(); // set the focus back to the input
+  }
+});
+
+function setChapterList() {
+  localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+  return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+chapter = chapter.slice(0, chapter.length - 1); // this slices off the last character
+
+chaptersArray = chaptersArray.filter((item) => item !== chapter);
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter(item => item !== chapter);
+  setChapterList();
+}
+
---
+++ lesson08/scripts/getdates.js
@@ -0,0 +1,26 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => { 
+  if (input.value !== '') { 
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus();
+    });
+
+    input.value = '';
+  } else {
+    window.alert("Please enter a chapter")
+  }
+  input.focus(); // Ensure input is focused regardless of the condition
+});
