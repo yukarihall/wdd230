45032168e680a79b1b266725514ab4fd785aa97a
Yukari Hall <yukarihall@gmail.com>
2024-06-27 13:53:02 -0600

    claresholm weather

--- scripts/weather.js
+++ scripts/weather.js
@@ -3,9 +3,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherDesc = document.querySelector('#weather-desc');
 const weatherIcon = document.querySelector('#weather-icon');
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=49.7497&lon=6.6371&units=imperial&appid=cc6cfeb43c3f002475c65affc1cf1526`;
-
-
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=50.02312&lon=-113.57168&units=imperial&appid=cc6cfeb43c3f002475c65affc1cf1526`;
 
 // handle errors
 async function apiFetch() {
